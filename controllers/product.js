07c20bffbc73e08f1c6af35b0bb7785f35f21339
sunil <sunilkumarbajpai.skb@gmail.com>
2020-09-26 19:42:36 +0530

    completed module 6.2

--- controllers/product.js
+++ controllers/product.js
@@ -141,7 +141,7 @@ exports.list = (req, res) => {
   let order = req.query.order ? req.query.order : "asc";
 
   let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
-  let limit = req.query.limit ? req.query.limit : 6;
+  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
   Product.find()
     .select("-photo")
